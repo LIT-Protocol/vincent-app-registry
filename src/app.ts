991b5c24def2867d0b01cea27a42d9eacb249637
Daryl Collins <daryl@litprotocol.com>
2025-02-21 08:10:15 +0000

    feat: Only start listen server once MongoDB connection succeeds (fail fast on initialization)

--- src/app.ts
+++ src/app.ts
@@ -10,15 +10,17 @@ const logger = consola.withTag('app init');
 dotenv.config();
 const app = express();
 app.use(express.json());
-
-mongoose
-  .connect(process.env.MONGODB_URI as string)
-  .then(() => logger.log('Connected to MongoDB'))
-  .catch((err) => logger.error('MongoDB connection error:', err));
-
 app.use('/api/v1', appRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  logger.log(`Server is running on port ${PORT}`);
-});
+
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => {
+    logger.log('Connected to MongoDB');
+
+    app.listen(PORT, () => {
+      logger.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => logger.error('MongoDB connection error:', err));
