135091fd3abc4cd13ed94c09e2993f1ea37f5f16
Ansh Saxena <anshsaxena4190@gmail.com>
2025-02-22 10:36:22 +0530

    feat: siwe auth

--- src/routes/appRoutes.ts
+++ src/routes/appRoutes.ts
@@ -1,4 +1,5 @@
-import { Router } from 'express';
+import cors from 'cors';
+import { Router, Request, Response, NextFunction } from 'express';
 
 import {
   registerApp,
@@ -11,13 +12,31 @@ import {
 
 export const appRouter = Router();
 
-// Define a type for the request handler
-// type RequestHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
+type RequestHandler = (req: Request, res: Response, next: NextFunction) => Promise<void>;
+
+const corsOptions = {
+  optionsSuccessStatus: 200,
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin) return callback(null, true);
+    const allowedOrigins = [
+      /^http:\/localhost(:\d+)?$/, // localhost with any port
+      new RegExp(`^https?://(.+.)?${process.env.DOMAIN}$`), // Any subdomain (optional) of the configured domain
+    ];
+    if (allowedOrigins.some((regex) => regex.test(origin))) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+// Apply CORS middleware to all routes in this router
+appRouter.use(cors(corsOptions));
 
 // Use the RequestHandler type to cast each controller function
-appRouter.post('/registerApp', registerApp);
-appRouter.get('/appMetadata/:appId', getAppMetadata);
-appRouter.put('/updateApp', updateApp);
-appRouter.post('/createRole', createRole);
-appRouter.get('/role/:appId/:roleId', getRole);
-appRouter.put('/updateRole', updateRole);
+appRouter.post('/registerApp', registerApp as RequestHandler);
+appRouter.get('/appMetadata/:appId', getAppMetadata as RequestHandler);
+appRouter.put('/updateApp', updateApp as RequestHandler);
+appRouter.post('/createRole', createRole as RequestHandler);
+appRouter.get('/role/:appId/:roleId', getRole as RequestHandler);
+appRouter.put('/updateRole', updateRole as RequestHandler);
