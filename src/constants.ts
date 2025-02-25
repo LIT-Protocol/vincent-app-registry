0b8f7641d7856e1c6fa9019837425097c8c1737d
Daryl Collins <daryl@litprotocol.com>
2025-02-25 06:45:12 +0000

    chore: Prefer heroku externally facing domain over local domain

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,8 @@
+import consola from 'consola';
+
 export const DOMAIN =
-  process.env.DOMAIN ||
   process.env.HEROKU_APP_DEFAULT_DOMAIN_NAME ||
+  process.env.DOMAIN ||
   (`localhost:${process.env.PORT}` as const);
+
+consola.log('DOMAIN IS:', DOMAIN);
